0eb04c15baa52dda1c4ec4cf221c45af6590390a
ivangmg <ivangmg@hotmail.com>
2023-03-19 17:05:40 -0700

    Thought model created

---
+++ models/Thought.js
@@ -0,0 +1,73 @@
+const { Schema, model, Types } = require('mongoose');
+
+//reaction schema
+const reactionSchema = new Schema (
+  {
+     reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+     },
+     reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+     },
+     username: {
+      type: String,
+      required: true,
+     },
+     createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => timeSince(date),
+     },
+  },
+  {
+      toJSON: {
+          virtuals: true,
+          getters: true
+      },
+      id: false,
+  }
+)
+
+// thought schema
+const thoughtSchema = new Schema (
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => timeSince(date),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+      toJSON: {
+          virtuals: true,
+          getters: true,
+      },
+      id: false,
+  }
+)
+
+
+// get total count of friends
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+})
+
+// create the User model using the UserSchema
+const Thought = model('Thought', thoughtSchema);
+// export the Thought model
+module.exports = Thought;
